45a0284b9064fa7c03264eb3aa2ab83e6a762446
Joy Boy <joyboy@Joys-MacBook-Air.local>
2024-05-20 16:19:47 -0700

    Opened side panel

--- background.js
+++ background.js
@@ -1,6 +1,1 @@
-const color = "#3aa757";
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log(`[Coloring] default background color is set to: ${color}`);
-});
+chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch((error)=>console.error(error))
